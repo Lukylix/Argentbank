28e5ce39e3537eb8f19f6b87ba785191b48883bb
Lukylix <dev.garcia.lucas@gmail.com>
2022-05-17 14:46:13 +0200

    Sign In Static

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -1,8 +1,37 @@
+import "./SignIn.css";
+
 export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = {};
+    for (const element of event.target.elements) {
+      if (element.nodeName === "INPUT") {
+        formData[element.id] = element.type === "checkbox" ? element.checked : element.value;
+      }
+    }
+    console.log(formData);
+  };
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>This is the profile page</p>
-    </div>
+    <main className="main bg-dark">
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <button className="sign-in-button">Sign In</button>
+        </form>
+      </section>
+    </main>
   );
 }
