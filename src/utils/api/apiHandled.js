e0c922db63b3bd593b8462e6fa3c1480aa001d3c
Lukylix <lucas.garcia.bobi@gmail.com>
2022-05-27 15:16:54 +0200

    Fix alert && Dry apiHandled

--- src/utils/api/apiHandled.js
+++ src/utils/api/apiHandled.js
@@ -7,12 +7,23 @@ import { setAccounts } from "../redux/accountsSlice";
 import { setTransactions, updateTransaction as actionUpdateTransaction } from "../redux/transactionsSlice";
 import { setCategories } from "../redux/categoriesSlice";
 
-const login = (email, password) => async (dispatch, navigate) => {
+const formatErrorMessage = (message) => message.replace(/^Error: /g, "");
+
+const classicErrorHandler = (error, dispatch, logout) => {
+  if (error?.status === 400) return dispatch(setAlert(formatErrorMessage(error.message), "warning"));
+  if (error?.status === 401) {
+    dispatch(setAlert("Session expired.", "warning"));
+    logout();
+    return;
+  }
+  if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
+  return dispatch(setAlert("Something went wrong.", "warning"));
+};
+
+const login = (email, password) => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.login(email, password);
   (() => {
-    if (error?.status === 400) return dispatch(setAlert("Invalid credentials.", "warning"));
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body?.token) {
       localStorage.setItem("token", data.body.token);
       dispatch(setToken(data.body.token));
@@ -25,13 +36,7 @@ const login = (email, password) => async (dispatch, navigate) => {
 const getUserProfile = (token) => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.getUserProfile(token);
   (() => {
-    if (error?.status === 400 || error?.status === 401) {
-      dispatch(setAlert("Session expired.", "warning"));
-      logout();
-      return;
-    }
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body) dispatch(setUser(data.body));
   })();
   return { data, error };
@@ -40,13 +45,7 @@ const getUserProfile = (token) => async (dispatch, navigate, logout) => {
 const updateUserProfile = (token, firstName, lastName) => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.updateUserProfile(token, firstName, lastName);
   (() => {
-    if (error?.status === 400 || error?.status === 401) {
-      dispatch(setAlert("Session expired.", "warning"));
-      logout();
-      return;
-    }
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body) dispatch(setUserNames({ firstName: data.body.firstName, lastName: data.body.lastName }));
   })();
   return { data, error };
@@ -55,13 +54,7 @@ const updateUserProfile = (token, firstName, lastName) => async (dispatch, navig
 const getAccounts = (token) => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.getAccounts(token);
   (() => {
-    if (error?.status === 400 || error?.status === 401) {
-      dispatch(setAlert("Session expired.", "warning"));
-      logout();
-      return;
-    }
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body) dispatch(setAccounts(data.body));
   })();
   return { data, error };
@@ -70,14 +63,7 @@ const getAccounts = (token) => async (dispatch, navigate, logout) => {
 const getTransactions = (token, accountId, queryPage) => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.getTransactions(token, accountId, queryPage);
   (() => {
-    if (error?.status === 400) return dispatch(setAlert("Account not found.", "warning"));
-    if (error?.status === 401) {
-      dispatch(setAlert("Session expired.", "warning"));
-      logout();
-      return;
-    }
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body) dispatch(setTransactions(data.body));
   })();
   return { data, error };
@@ -86,25 +72,16 @@ const getTransactions = (token, accountId, queryPage) => async (dispatch, naviga
 const updateTransaction = (token, accountId, transationId, update) => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.updateTransaction(token, accountId, transationId, update);
   (() => {
-    if (error?.status === 400) return dispatch(setAlert("Account not found.", "warning"));
-    if (error?.status === 401) {
-      dispatch(setAlert("Session expired.", "warning"));
-      logout();
-      return;
-    }
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body) dispatch(actionUpdateTransaction(data.body));
   })();
   return { data, error };
 };
 
-const getCategories = () => async (dispatch) => {
+const getCategories = () => async (dispatch, navigate, logout) => {
   const { data, error } = await apiEndpoints.getCategories();
   (() => {
-    if (error?.status === 400) return dispatch(setAlert("Categories not found.", "warning"));
-    if (error?.status === 500) return dispatch(setAlert("Internal server error.", "danger"));
-    if (error) return dispatch(setAlert("Something went wrong.", "warning"));
+    if (error) return classicErrorHandler(error, dispatch, logout);
     if (data?.body) dispatch(setCategories(data.body));
   })();
   return { data, error };
