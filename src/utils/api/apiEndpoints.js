c418592a418c8e78288c1af81c498cc2f77ef21d
Lukylix <dev.garcia.lucas@gmail.com>
2022-11-17 17:18:10 +0100

    Vite

--- src/utils/api/apiEndpoints.js
+++ src/utils/api/apiEndpoints.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const host = process.env.REACT_APP_API_HOST || "localhost";
-const port = process.env.REACT_APP_API_PORT || 3001;
+const host = import.meta.env.VITE_APP_API_HOST || "localhost";
+const port = import.meta.env.VITE_APP_API_PORT || 3001;
 
 const client = axios.create({
   baseURL: `http://${host}:${port}/api/v1`,
